f13913d8b22255adce6d6059f434ad1a8c6aaa8e
kalaiaras <kalaiyarasan8rim@gmail.com>
2023-12-26 08:09:19 +0530

    frondend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import Nav from './Nav'
 import './App.css';
-
+import {BrowserRouter,Route,Routes} from 'react-router-dom'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+      <Nav/>
+     <Routes>
+      <Route path ='/' element={<h1>Product</h1>}></Route>
+      <Route path ='/add' element={<h1> ADD Product</h1>}></Route>
+      <Route path ='/update' element={<h1> UPDATE Product</h1>}></Route>
+      <Route path ='/logout' element={<h1>logout</h1>}></Route>
+      <Route path ='/profile' element={<h1>profile</h1>}></Route>
+
+     </Routes>
+      </BrowserRouter>
     </div>
   );
 }
